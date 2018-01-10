363c22f5c2d8725cc205bb04bf3523eef2a1ab45
AndIMissU <784738601@qq.com>
2018-01-10 18:32:58 +0800

    vue的nexttick的学习

---
+++ vue/qs_vue/try-vue-nexttick/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <div>姓名:{{name}}</div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      name:''
+    }
+ 
+  },
+  beforeCreate() {
+    // 在页面渲染前设置数据 太早了 不好 数据还是初始化的数据
+    this.$nextTick(() => {
+      console.log('aaaa');
+      this.name = "hjakda";
+    })
+  },
+  mounted () {
+    this.name = '哈哈哈哈哈哈哈';
+    console.log("jhkala");
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
