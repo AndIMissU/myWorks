6dee5d4d4be660c476a53dfadfcc59c29579d841
AndIMissU <784738601@qq.com>
2018-01-12 21:22:36 +0800

    step 页面 el组件学习

--- vue/qs_vue/try-element-ui/src/router/index.js
+++ vue/qs_vue/try-element-ui/src/router/index.js
@@ -3,6 +3,10 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import totalpages from '@/components/activeManage'
 import activePublic from '@/components/activePublic'
+import step1 from '@/page/activePublic/step1.vue'
+import step2 from '@/page/activePublic/step2.vue'
+import step3 from '@/page/activePublic/step3.vue'
+import step4 from '@/page/activePublic/step4.vue'
 
 Vue.use(Router)
 
@@ -19,7 +23,29 @@ export default new Router({
     },
     {
       path: '/activePublic',
-      component: activePublic
+      component: activePublic,
+      children: [
+        {
+          path: '',
+          component: step1
+        },
+        {
+          path: 'step1',
+          component: step1
+        },
+        {
+          path: 'step2',
+          component: step2
+        },
+        {
+          path: 'step3',
+          component: step3
+        },
+        {
+          path: 'step4',
+          component: step4
+        }
+      ]
     }
   ]
 })
