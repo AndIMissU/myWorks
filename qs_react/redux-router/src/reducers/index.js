1038c38edb4a988298090c73e484eeafe6a4782f
AndIMissU <784738601@qq.com>
2018-03-13 16:15:22 +0800

    redux-router 的学习

---
+++ qs_react/redux-router/src/reducers/index.js
@@ -0,0 +1,8 @@
+// reducers 是一个纯函数 旧状态 + 参数 返回新状态
+export default (state = 0, action) => {
+  switch(action.type) {
+    case 'INCREMENT': return state + 1;
+    case 'DECREMENT': return state - 1;
+    default: return state;
+  }
+}
